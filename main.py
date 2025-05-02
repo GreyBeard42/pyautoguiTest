61e9252ed6fca5f0321ef388e15cfde320202c8a
GreyBeard42 <greybeard4200@yahoo.com>
2025-05-01 19:39:55 -0700

    v1.1 - Dark Mode

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def findImage(path):
     my = cv2.minMaxLoc(result)[3][1]
     return mx, my, cv2.minMaxLoc(result)[0]
 
-openchrome = findImage("chrome.png")
+openchrome = findImage(r"C:\Users\greys\OneDrive\Desktop\code\pyautoguiTest\chrome.png")
 pyautogui.moveTo(openchrome[0], openchrome[1])
 pyautogui.click()
 pyautogui.moveTo(openchrome[0], openchrome[1]-75)
@@ -35,18 +35,21 @@ time.sleep(1)
 
 waiting = True
 while waiting:
-    searchbar = findImage("ytsearch.png")
+    searchbar = findImage(r"C:\Users\greys\OneDrive\Desktop\code\pyautoguiTest\ytsearch.png")
     pyautogui.moveTo(searchbar[0], searchbar[1])
     if searchbar[1] < h/2:
         waiting = False
     time.sleep(0.5)
 print("youtube loaded")
 
-searchbar = findImage("ytsearch.png")
+time.sleep(2)
+searchbar = findImage(r"C:\Users\greys\OneDrive\Desktop\code\pyautoguiTest\ytsearch.png")
 
 pyautogui.moveTo(searchbar[0]+100, searchbar[1]+30)
 pyautogui.click()
-pyautogui.typewrite(['l','o','f','i','ENTER'], interval=0)
+time.sleep(0.1)
+pyautogui.typewrite("lofi live", interval=0.1)
+pyautogui.hotkey("ENTER")
 print("searched for lofi")
 
 time.sleep(1)
@@ -55,7 +58,7 @@ pyautogui.moveTo(w-10, searchbar[1]+30)
 waiting = True
 while waiting:
     pyautogui.scroll(-500)
-    live = findImage("live.png")
+    live = findImage(r"C:\Users\greys\OneDrive\Desktop\code\pyautoguiTest\live.png")
     if live[0] > 800 and live[1] > 300:
         pyautogui.moveTo(live[0]-100, live[1])
         waiting = False
